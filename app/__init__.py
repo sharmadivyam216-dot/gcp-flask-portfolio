a4db546921c97ea00cd818bdd5c045b790444d49
Divyam Sharma <sharmadivyam216@gmail.com>
2026-07-04 22:22:04 +0530

    Add custom error pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from app.config import Config
 
 
@@ -9,5 +9,13 @@ def create_app():
 
     from app.routes import main
     app.register_blueprint(main)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("404.html"), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template("500.html"), 500
 
     return app

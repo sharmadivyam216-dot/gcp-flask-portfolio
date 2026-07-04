84949e18bec52832db39b220828dcb4a7e062567
Divyam Sharma <sharmadivyam216@gmail.com>
2026-07-04 22:13:21 +0530

    Add health check endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/")
 def home():
     return render_template("index.html")
 
+
 @main.route("/about")
 def about():
     return render_template("about.html")
+
+
+@main.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    }), 200

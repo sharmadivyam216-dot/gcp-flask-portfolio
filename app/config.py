b919e310198c7eac47fd12be80e2e7fdc533091c
Divyam Sharma <sharmadivyam216@gmail.com>
2026-07-04 22:05:13 +0530

    Add environment based configuration with python-dotenv

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
-    DEBUG = os.getenv("DEBUG", "True") == "True"
